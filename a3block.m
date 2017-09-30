22116a7ddd6fcee09997966410dcb52d2922edf3
Arpit Aggarwal <ar13pit@gmail.com>
2017-10-01 01:01:04 +0200

    implementation plan added

--- a3block.m
+++ a3block.m
@@ -1,15 +1,26 @@
 classdef a3block
+    %A3BLOCK Class definition of a3block Objects used to create a3model.
+    %   a3block returns 1 if S is an a3block Object and 0 otherwise.
+
+    % Implement this as node of a directed graph
     properties
+        Name
         BlockType
     end
     methods
-        function obj=a3block(name)
+        function obj=a3block(blockname,blocktype)
             if nargin > 0
-                if ischar(name)
-                    obj.BlockType=name;
+                if ischar(blockname)
+                    obj.Name=blockname;
                 else
-                    error('a3block name has to be a character array')
+                    error('a3block Name has to be a character array')
                 end
+                if ischar(blocktype)
+                    obj.BlockType=blocktype;
+                else
+                    error('a3block BlockType has to be a character array')
+                end
+                
             else
                 error('Not enough input arguments to create a3block Object')
             end
--- a3model.m
+++ a3model.m
@@ -2,6 +2,19 @@ classdef a3model
     %A3MODEL A Class definition to programatically create and simulate Simulink models
     %   a3model returns 1 if S is an a3block Object and 0 otherwise.
 
+% Implement this as a directed graph. To get the right postion of the
+% blocks, calculate the longest connection between the input and output
+% nodes such that each node occurs only once. Place the nodes of this
+% connection on one line. Place the nodes that are in the same direction as the
+% longest connection above it and the ones in a direction opposite below
+% it. (This represents feedforward and feedback)
+
+% The search for the longest connection starts from a node that has a
+% previous node value as null and ends at a node which has the next node
+% value as null. (Representing Input and Output) (Disturbances and some
+% other nodes may satisfy this condition but the path will be longest only
+% in the case of the initial input and the final output
+
     properties
         ModelName
         BlockObjects
@@ -11,12 +24,15 @@ classdef a3model
             if nargin > 0
                 if ischar(name)
                     obj.ModelName = name;
+                    new_system(obj.ModelName);
                 else
                     error('ModelName must be string')
                 end
                 if isa3block(a3blockObject)
                     obj.BlockObjects=a3blockObject;
                 end
+            else
+                error('Not enough input arguments to create a3model object')
             end
         end
 %         function r = roundOff(obj)
@@ -31,8 +47,8 @@ classdef a3model
         function insert_block(a3blockObject)
             % Add the block to the linked list
         end
-        function simulate()
-            % Add code to run simulation
+        function simOut=simulate()
+            % Add code to run simulation and return a a3output Object
         end
         
             
